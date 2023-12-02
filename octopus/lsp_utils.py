c7e6e95fc9047afd007116c851786bd37ab75a78
Jean-Baptiste Doderlein <jean-baptiste.doderlein@ens-rennes.fr>
2023-12-02 17:37:19 +0100

    Get token info

--- octopus/lsp_utils.py
+++ octopus/lsp_utils.py
@@ -1,8 +1,24 @@
 from typing import Optional
 from octopus.lex import lexer
+from ply.lex import LexToken
 
 
-def get_info(doc : str, position) -> Optional[str]:
+def to_lexpos(text : str, position) -> int:
+    return sum(map(len,text.split("\n")[:position.line])) + position.character
+
+def find_token(doc : str, position):
     lexer.input(doc)
     tokens = [t for t in lexer.token()]
+    s_pos = to_lexpos(doc, position)
+    for i in range(len(tokens)-1):
+        if tokens[i].lexpos <= s_pos and tokens[i].lexpos > s_pos:
+            return tokens[i]
+    return None
+
+def get_info(doc : str, position) -> Optional[str]:
+    token = find_token(doc, position)
+    if token:
+        match token:
+            case LexToken(type="TANTACULE"):
+                return "***ATTENTION***"
     return None
