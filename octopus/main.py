5f2a628ffe1d989cdd22751e68b7c9528a3360ba
Jean-Baptiste Doderlein <jean-baptiste.doderlein@ens-rennes.fr>
2023-12-02 11:15:45 +0100

    Add outptu flag

--- octopus/main.py
+++ octopus/main.py
@@ -7,6 +7,8 @@ def main():
     arg_parser = argparse.ArgumentParser(description='Compile ant')
     arg_parser.add_argument('input', type=str, 
                     help='File to compile')
+    arg_parser.add_argument('--output', "-o", type=str, 
+                    help='File to store')
     args = arg_parser.parse_args()
     with open(os.path.abspath(args.input), 'r') as f:
         input_string = f.read()
@@ -15,11 +17,15 @@ def main():
         visitor = CodeGenVisitor()
         visitor.visit(expression)
 
-        print(expression)
+        #print(expression)
 
         assembly_code = visitor.get_code()
-        print("Produced code:")
-        print(assembly_code)
+        if args.output:
+            with open(args.output,'w') as fo:
+                fo.write(assembly_code)
+        else:
+            print("Produced code:")
+            print(assembly_code)
 
 if "__main__" in __name__:
     main()
