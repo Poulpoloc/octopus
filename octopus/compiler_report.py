0c0cfe040b9de36d8744d7ba2d6928e17134d9c7
Jean-Baptiste Doderlein <jean-baptiste.doderlein@ens-rennes.fr>
2023-12-02 15:34:55 +0100

    correct type warning

--- octopus/compiler_report.py
+++ octopus/compiler_report.py
@@ -1,29 +1,3 @@
-class CompilerReport:
-    """
-    Contains useful information about the compilation process.
-    This is supposed to be used by Poulpi.
-    """
-    warnings : list[Warning] = []
-    errors = []
-    def __init__(self):
-        pass
-
-    def reset(self):
-        self.warnings = []
-        self.errors = []
-
-    def warning(self, warning:Warning):
-        self.warnings.append(warning)
-
-    def error(self, error):
-        self.errors.append(error)
-
-    def merge_span(self, spans):
-        return min([i[0] for i in spans]), max([i[1] for i in spans])
-
-    def __repr__(self):
-        return f"REPORT:\n{self.warnings}\n{self.errors}"
-
 class CRWarning:
     def __init__(self, message, location_span):
         self.message = message
@@ -39,3 +13,32 @@ class CRError:
 
     def __repr__(self):
         return f"ERROR: {self.message} at {self.location_span}"
+
+
+class CompilerReport:
+    """
+    Contains useful information about the compilation process.
+    This is supposed to be used by Poulpi.
+    """
+    warnings : list[CRWarning] = []
+    errors : list[CRError]= []
+    def __init__(self):
+        pass
+
+    def reset(self):
+        self.warnings = []
+        self.errors = []
+
+    def warning(self, warning:CRWarning):
+        self.warnings.append(warning)
+
+    def error(self, error):
+        self.errors.append(error)
+
+    def merge_span(self, spans):
+        return min([i[0] for i in spans]), max([i[1] for i in spans])
+
+    def __repr__(self):
+        return f"REPORT:\n{self.warnings}\n{self.errors}"
+
+
