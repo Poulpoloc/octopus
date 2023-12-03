b0dd36fc36ace4d211fa5c4abb18d85e6e0309b3
Jean-Baptiste Doderlein <jean-baptiste.doderlein@ens-rennes.fr>
2023-12-03 08:41:53 +0100

    personnal optim

--- octopus/iroptim.py
+++ octopus/iroptim.py
@@ -7,8 +7,15 @@ class IRGotoOptimizer(IRVisitor):
 
     def visit_ir(self, ir: IR):
         self.ir = ir
-        for bloc in sorted(ir.blocs, key=lambda b: len(b.predecessors)):
-            self.visit(bloc)
+        before,after = len(ir.blocs), len(ir.blocs)-1
+        while before!=after: # Point fixe
+            print("IR Loop")
+            before = len(ir.blocs)
+            self.to_delete = []
+            for bloc in sorted(ir.blocs, key=lambda b: len(b.predecessors)):
+                self.visit(bloc)
+            ir.blocs = [b for b in self.ir.blocs if not b in self.to_delete]
+            after = len(ir.blocs)
 
 
     def visit_bloc(self, bloc: Bloc):
@@ -19,7 +26,7 @@ class IRGotoOptimizer(IRVisitor):
                     goto.target.predecessors.remove(bloc)
                 except:
                     pass
-                self.ir.blocs.remove(bloc)
+                self.to_delete.append(bloc)
                 if bloc == self.ir.main_bloc:
                     self.ir.main_bloc = goto.target
                 
@@ -37,4 +44,31 @@ class IRGotoOptimizer(IRVisitor):
                         end.add_predecessor(start)
                         # Replace goto by end in start terminator
                         start.get_terminator().replace(bloc,end)
-                    self.ir.blocs.remove(bloc)
+                    self.to_delete.append(bloc)
+        elif len(bloc.instructions) == 1 and len(bloc.predecessors) == 0 and bloc != self.ir.main_bloc: # Dead Code elimination
+            termin = bloc.get_terminator()
+            if hasattr(termin, "target"):
+                try: termin.target.predecessors.remove(bloc)
+                except: pass
+                self.to_delete.append(bloc)
+                if bloc == self.ir.main_bloc:
+                    self.ir.main_bloc = termin.target
+            elif hasattr(termin, "then"):
+                try: termin.then.predecessors.remove(bloc)
+                except: pass
+                try: termin.else_.predecessors.remove(bloc)
+                except: pass
+                self.to_delete.append(bloc)
+                if bloc == self.ir.main_bloc:
+                    self.ir.main_bloc = termin.then
+            elif hasattr(termin, "follower"):
+                try: termin.follower.predecessors.remove(bloc)
+                except: pass
+                try: termin.handler.predecessors.remove(bloc)
+                except: pass
+                self.to_delete.append(bloc)
+                if bloc == self.ir.main_bloc:
+                    self.ir.main_bloc = termin.follower
+            
+
+        
